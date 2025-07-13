76412810091c09772303197253668313086c6fd2
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-07-13 23:20:14 +0200

    Maken oefening 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-};
+    const cleanedString = string
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join("");
 
+    console.log(cleanedString);
+
+    const reversedString = cleanedString.split('').reverse().join('');
+
+
+    //controleren of beide strings overeenkomen
+    if(cleanedString === reversedString) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}; 
+
+console.log(palindromes("Racecar!"));
 // Do not edit below this line
 module.exports = palindromes;
