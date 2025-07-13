cb7787ac0496bd41eb469367674069926ceec987
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-07-13 23:01:35 +0200

    Maken oefening 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((prev, current) => prev + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, current) => prev * current);
 };
 
-const power = function() {
-	
+multiply([5,10]);
+
+const power = function(number1, number2) {
+	return number1 ** number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+
+  for (let i = 1; i <= number; i++) {
+    result = result * i;
+
+  }
+  
+	return result;
 };
+
+factorial(2);
 
 // Do not edit below this line
 module.exports = {
