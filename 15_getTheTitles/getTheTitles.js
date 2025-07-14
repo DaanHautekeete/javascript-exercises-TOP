1c9e8204cca785d5e4326877b42df8efc3443024
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-07-14 21:49:14 +0200

    Maken oefening 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let array = Array.from(books);
+    let arrayTitles = [];
+    
+    array.map((book) => {arrayTitles.push(book.title);});
+    return arrayTitles;
 };
+
+    const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
